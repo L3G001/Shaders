f7ab1777d51450aea6d16d315be8697e7a895d56
LEGO <matias.labreniuk@davinci.edu.ar>
2024-10-17 21:27:15 -0300

    Day & Night Cycle Skybox Shader

---
+++ Assets/Art/Shader Library/Shaders/1 - Ambient/Sky/Day & Night Cycle/GetMainLightDirection.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMainLightDirection : MonoBehaviour
+{
+    [SerializeField] private Material _skyboxMaterial;
+    [SerializeField] private Light _mainLight;
+
+    private void Update()
+    {
+        _skyboxMaterial.SetVector("_MainLightDirection", _mainLight.transform.forward);
+        _skyboxMaterial.SetVector("_MainLightUp", _mainLight.transform.up);
+        _skyboxMaterial.SetVector("_MainLightRight", _mainLight.transform.right);
+    }
+}
